c7c6d587a5a16414bd624130eed99e3ec28eaf82
racdale <racdale@gmail.com>
2015-04-03 16:31:06 -0700

    fixed renderauthors

--- really-really-simple-bib2json-functions.php
+++ really-really-simple-bib2json-functions.php
@@ -69,7 +69,7 @@ function renderAuthor($authString) { # can make simple assumptions about how you
 		for ($i=0;$i<count($arr);$i++) {
 			if ($i==count($arr)-1 & count($arr)>1) {
 				 # remove all 'ands' but convert last one to ampersand (APA format)
-				$auth .= substr($auth,0,strlen($auth)-2).' &amp; '.$arr[$i];
+				$auth = substr($auth,0,strlen($auth)-2).' &amp; '.$arr[$i];
 			} else {
 				$auth .= $arr[$i].', ';
 			}
